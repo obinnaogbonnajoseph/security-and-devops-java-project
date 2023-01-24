3048d5ae3e12fa00276cf799ff00827595a11d5b
Obinna Ogbonna <obinnaogbonnajoseph@gmail.com>
2023-01-24 14:59:01 +0100

    add lombok

--- src/main/java/com/example/demo/model/persistence/User.java
+++ src/main/java/com/example/demo/model/persistence/User.java
@@ -1,21 +1,15 @@
 package com.example.demo.model.persistence;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.persistence.*;
 
 
 @Entity
 @Table(name = "user")
+@Data
 public class User {
 
 	@Id
@@ -41,29 +35,5 @@ public class User {
 	@JoinColumn(name = "cart_id", referencedColumnName = "id")
 	@JsonIgnore
 	private Cart cart;
-
-	public Cart getCart() {
-		return cart;
-	}
-
-	public void setCart(Cart cart) {
-		this.cart = cart;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
 	
 }
